4532836b843a1f609bd2747f0f92776ea6cbb63a
kheldae <taho.gougeon@gmail.com>
2025-03-13 11:50:41 +0100

    updated lsd icons

--- 00-prereqs.zsh
+++ 00-prereqs.zsh
@@ -125,6 +125,7 @@ function __deps::check_optional() {
     __deps::check   jsonnet      jsonnet
     __deps::check   pgcli        pgcli
     __deps::check   darkhttpd    darkhttpd
+    __deps::check   sops         sops
 }
 
 # Check if user cloned submodules, and ask if we can do it for them.
