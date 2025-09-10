d945c559a0ee54fe9e05cf72c8fbb057a00767ac
kheldae <taho.gougeon@gmail.com>
2025-09-10 10:02:15 +0200

    other placement fixes

--- 20-functions.zsh
+++ 20-functions.zsh
@@ -71,7 +71,7 @@ function duf() {
 }
 
 function fetch() {
-    [[ $TERM == xterm-kitty ]] && kitty @resize-os-window --height 28
+    [[ $TERM == xterm-kitty ]] && kitty @resize-os-window --height 27
     clear && fastfetch --logo ${c_fetch_image} --logo-type file
 }
 
