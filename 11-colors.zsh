1290797f76f97b8d94eec2fe5c84d729c9ac2cb5
kheldae <taho.gougeon@gmail.com>
2026-09-09 16:42:13 +0200

    colors

--- 11-colors.zsh
+++ 11-colors.zsh
@@ -15,7 +15,7 @@ c_kitty_opacity[light]="0.8"
 
 # Colorscheme
 colors_light[goyo_bg]="#d9d9da"
-colors_dark[goyo_bg]="#574c2e"
+colors_dark[goyo_bg]="#3c3c3c"
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=10"
 
